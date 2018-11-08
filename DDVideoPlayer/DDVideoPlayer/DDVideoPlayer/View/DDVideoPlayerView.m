b65162fe6b127f8cd9b06ff6a47d69ad90f972d3
wuqihan <wuqh_ios@icloud.com>
2018-11-08 14:55:50 +0800

    add DDPlayer
    OC写着太难受 ，还是试试swift吧

---
+++ DDVideoPlayer/DDVideoPlayer/DDVideoPlayer/View/DDVideoPlayerView.m
@@ -0,0 +1,21 @@
+//
+//  DDVideoPlayerView.m
+//  DDVideoPlayer
+//
+//  Created by wuqh on 2018/11/8.
+//  Copyright © 2018 wuqh. All rights reserved.
+//
+
+#import "DDVideoPlayerView.h"
+
+@implementation DDVideoPlayerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
